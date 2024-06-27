3efd2d6b040730565fa3d3e8dd9b0fc348027236
williamtuominiemi <william.m.tuominiemi@gmail.com>
2024-06-27 22:25:56 +0300

    tokenize and stem

--- util.py
+++ util.py
@@ -1,7 +1,7 @@
 import nltk
 # nltk.download('punkt')
-from nltk.stem.porter import PorterStemmer
-stemmer = PorterStemmer()
+from nltk.stem import SnowballStemmer
+stemmer = SnowballStemmer("english")
 
 def tokenize(sentence):
     return nltk.word_tokenize(sentence)
@@ -12,7 +12,12 @@ def stem(word):
 def bag_of_words(tokenized_sentence, all_words):
     pass
 
-a = "How do I download the application"
-print(a)
-a = tokenize(a)
-print(a)
+# a = "How do I download the application"
+# print(a)
+# a = tokenize(a)
+# print(a)
+
+words = ["connects", "connected", "connecting"]
+stemmed_words = [stemmer.stem(word) for word in words]
+print(stemmed_words)
+
