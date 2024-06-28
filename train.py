bcbc8a89aa06e00dd7c0988794ebafe9a04401bc
williamtuominiemi <william.m.tuominiemi@gmail.com>
2024-06-28 23:00:45 +0300

    train model

--- train.py
+++ train.py
@@ -6,6 +6,8 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 
+from model import NeuralNet
+
 with open('intents.json', 'r') as f:
     intents = json.load(f)
 
@@ -44,13 +46,43 @@ class ChatDataset(Dataset):
         self.x_data = X_train
         self.y_train = y_train
 
-    def __getitem__(self, idx):
-        return self.x_data[idx], self.y_data[idx]
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_train[index]
 
     def __len__(self):
         return self.n_samples
 
 batch_size = 8
+input_size = len(X_train[0])
+hidden_size = 8
+output_size = len(tags)
+learning_rate = 0.001
+num_epochs = 1500
 
 dataset = ChatDataset()
-train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+for epoch in range(num_epochs):
+    for (words, labels) in train_loader:
+        words = words.to(device)
+        labels = labels.to(device, dtype=torch.long)
+        # forward
+        outputs = model(words)
+        loss = criterion(outputs, labels)
+
+        # backwards and optimizer
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    if(epoch+1)%100 == 0:
+        print(f'epoch {epoch+1}/{num_epochs}, loss={loss.item():.4f}')
+
+print(f'final loss, loss={loss.item():.4f}')
+
--- util.py
+++ util.py
@@ -19,8 +19,3 @@ def bag_of_words(tokenized_sentence, all_words):
 
     return bag
 
-sentence = ["hello", "how", "are", "you"]
-words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
-bag = bag_of_words(sentence, words)
-print(bag)
-
